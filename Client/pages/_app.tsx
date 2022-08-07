7ddb232a2b7cb55a50a93eac16954dbb25a8c885
Foebry <rain_fabry@hotmail.com>
2022-08-07 22:09:02 +0200

    remove unused Header component

--- Client/pages/_app.tsx
+++ Client/pages/_app.tsx
@@ -1,6 +1,5 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import Header from "../components/Header";
 import { Nav } from "../components/Nav";
 import Footer from "../components/Footer";
 import AppProvider from "../context/appContext";
