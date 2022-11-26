21f9b8a5859dbfb4c10c56ce0526764ca83a043d
Foebry <rain_fabry@hotmail.com>
2022-11-26 22:15:49 +0100

    feat: admin/honden + hondDetail

---
+++ Client/pages/api/admin/honden/index.ts
@@ -0,0 +1,40 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { HOND } from "../../../../controllers/HondController";
+import {
+  getPaginatedData,
+  PaginatedRequestQuery,
+  PaginatedResponse,
+} from "../../../../helpers/RequestHelper";
+import {
+  mapToHondenOverviewResult,
+  PaginatedKlantHond,
+} from "../../../../middleware/mappers/honden";
+import {
+  HondCollection,
+  KlantHond,
+} from "../../../../types/EntityTpes/HondTypes";
+import { GenericRequest } from "../../auth/login";
+
+type ListHondenRequest = {
+  query: PaginatedRequestQuery;
+  url: string;
+};
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === "GET")
+    return getHondenOverview(req as GenericRequest<ListHondenRequest>, res);
+  return res.status(405).send("Not allowed");
+};
+
+const getHondenOverview = async (
+  req: GenericRequest<ListHondenRequest>,
+  res: NextApiResponse<PaginatedResponse<PaginatedKlantHond>>
+) => {
+  const data = await getPaginatedData<KlantHond>(req.query, req.url, HOND);
+
+  const result = mapToHondenOverviewResult(data);
+
+  return res.status(200).send(result);
+};
+
+export default handler;
