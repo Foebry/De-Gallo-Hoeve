6fdd3f46b428ac94520b18a86008ae6e93522cb0
Foebry <rain_fabry@hotmail.com>
2022-12-04 10:34:34 +0100

    fix: try fixing index.html not found

--- Client/next.config.js
+++ Client/next.config.js
@@ -4,7 +4,13 @@ const nextConfig = {
   images: {
     domains: ["www.wdev2.be", "res.cloudinary.com"],
   },
+  // trailingSlash: true,
   pageExtensions: ["page.tsx", "page.ts"],
+  exportPathMap: async (defaultPathMap) => {
+    return {
+      "/index/index/index.html": { page: "/" },
+    };
+  },
   rewrites: async () => {
     return [
       {
