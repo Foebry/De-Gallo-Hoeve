6818c3870bf9b47ae1e47f662139b1ff4e6109a0
Foebry <rain_fabry@hotmail.com>
2023-07-07 22:15:52 +0200

    fix: bezoeken and not uitlaat beurten

--- src/pages/home/components/diensten/ServiceCard.tsx
+++ src/pages/home/components/diensten/ServiceCard.tsx
@@ -35,9 +35,16 @@ const ServiceCard: React.FC<Props> = ({ active = false, title, imageSrc }) => {
           />
         </div>
         <div className="px-2 flex flex-col gap-2">
-          <Body>Gaat u op vakantie voor enkele dagen of langer en heeft u geen oplossing voor uw hond?</Body>
-          <Body>Heeft u het te druk met het werk en geen tijd om uw hond uit te laten?</Body>
-          <Body>Bent u slecht te been, of heeft u graag hulp bij het uitlaten van uw hond?</Body>
+          <Body>
+            Gaat u op vakantie voor enkele dagen of langer en heeft u geen oplossing voor
+            uw hond?
+          </Body>
+          <Body>
+            Heeft u het te druk met het werk en geen tijd om uw hond uit te laten?
+          </Body>
+          <Body>
+            Bent u slecht te been, of heeft u graag hulp bij het uitlaten van uw hond?
+          </Body>
           <Body>Dan staan wij iedere dag voor u klaar om u bij te staan.</Body>
         </div>
         <ul className="pl-5 md:pl-20 mt-10 mb-10">
@@ -48,14 +55,18 @@ const ServiceCard: React.FC<Props> = ({ active = false, title, imageSrc }) => {
             <GiCheckMark /> <Body>Bij u thuis</Body>
           </li>
           <li className="flex gap-2">
-            <GiCheckMark /> <Body>Tot 3 uitlaat-beurten mogelijk per dag</Body>
+            <GiCheckMark /> <Body>Tot 3 bezoeken mogelijk per dag</Body>
           </li>
           <li className="flex gap-2">
             <GiCheckMark /> <Body>€15 tot €35 per dag per hond</Body>
           </li>
         </ul>
         <FormRow className="absolute w-full bottom-2">
-          <Button className="mx-auto bg-grey-200 border-grey-200" label="aanvragen" disabled={true} />
+          <Button
+            className="mx-auto bg-grey-200 border-grey-200"
+            label="aanvragen"
+            disabled={true}
+          />
         </FormRow>
       </div>
     </div>
